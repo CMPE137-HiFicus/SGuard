8a7af04d5429e72f389f8ae00871260ef0f051b9
Kham Tran <khamtran1990@gmail.com>
2019-11-24 13:29:50 -0800

    modify files

--- SGuard/contact/Contact.swift
+++ SGuard/contact/Contact.swift
@@ -14,6 +14,7 @@ class Contact: UIViewController,UITableViewDataSource  {
     @IBOutlet weak var contact: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        contact.reloadData()
         contact.dataSource = self
     }
     
